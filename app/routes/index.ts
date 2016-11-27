01d524014b8789dbcc1bb4b8e15530de57112246
Aaron Sky <asky@sapient.com>
2016-11-27 17:59:28 -0500

    Added sync endpoint back to app

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,6 +1,7 @@
 
 import * as express from 'express';
 import * as moment from 'moment';
+import { Console, Slack } from '../logger';
 
 export function applyRoutes(webserver: express.Application, controller: botkit.Controller) {
     // controller.createWebhookEndpoints(webserver);
