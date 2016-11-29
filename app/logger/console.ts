7807aab17618d48c20301de14e13b374b5a0eb70
Aaron Sky <asky@sapient.com>
2016-11-28 21:38:13 -0500

    Finally fixed console logging for real this time

--- app/logger/console.ts
+++ app/logger/console.ts
@@ -1,38 +1,52 @@
 const winston = require('winston');
 
-const customLevels = {
-  levels: winston.config.syslog.levels,
-  colors: {
-    silly: 'pink'
-  }
-};
-const syslogLevels = Object.keys(winston.config.syslog.levels);
-customLevels.levels['silly'] = winston.config.syslog.levels[syslogLevels[syslogLevels.length - 1]] + 1;
+const logger = new winston.Logger({
+  level: 'info'
+});
 
-winston.remove(winston.transports.Console);
-winston.setLevels(customLevels.levels);
-winston.addColors(customLevels.colors);
-winston.cli();
+logger.cli();
 
 if (!process.env.TEST) {
-  winston.configure({
-    transports: [
-      new (winston.transports.Console)({
-        level: 'silly',
-        levels: customLevels.levels,
-        prettyPrint: true,
-        colorize: true,
-        timestamp: () => {
-          return new Date();
-        },
-        formatter: (options) => {
-          return `[Ibizan] (${options.timestamp()}) ${options.level.toUpperCase()}: ${!!options.message ? options.message : ''} ${(options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '')}`;
-        }
-      })
-    ]
+  const customLevels = {
+    levels: {
+      emerg: 0,
+      alert: 1,
+      crit: 2,
+      error: 3,
+      warning: 4,
+      notice: 5,
+      silly: 6,
+      info: 7,
+      debug: 8
+    },
+    colors: {
+      emerg: 'red',
+      alert: 'yellow',
+      crit: 'red',
+      error: 'red',
+      warning: 'red',
+      notice: 'yellow',
+      silly: 'pink',
+      info: 'green',
+      debug: 'blue'
+    }
+  };
+  logger.add(winston.transports.Console, {
+    level: 'info',
+    levels: customLevels.levels,
+    prettyPrint: true,
+    colorize: true,
+    timestamp: () => {
+      return new Date();
+    },
+    formatter: (options) => {
+      return `[Ibizan] (${options.timestamp()}) ${options.level.toUpperCase()}: ${!!options.message ? options.message : ''} ${(options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '')}`;
+    }
   });
+  logger.setLevels(customLevels.levels);
+  winston.addColors(customLevels.colors);
 }
 
 export {
-  winston as ConsoleLogger
+  logger as ConsoleLogger
 };
